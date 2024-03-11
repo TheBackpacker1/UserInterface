04dcdfd849862deca622312fd9291f61cd19ec7f
mohamed <mohamedlouati002@gmail.com>
2024-03-11 19:26:55 +0100

    sideBar Fonction

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,130 +1,92 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { Menu } from 'primereact/menu';
 import { Toolbar } from 'primereact/toolbar';
 import { Link } from 'react-router-dom';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { Sidebar } from 'primereact/sidebar';
 import PropTypes from 'prop-types';
-
+import './Layout.css';
+import { Badge } from 'primereact/badge'
 const Layout = ({ children }) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const menuRef = useRef(null);
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
+  const [visible, setVisible] = useState(true);
   const handleClose = () => {
-    setAnchorEl(null);
-  };
-
+    setVisible(false)
+  }
   const rightItems = [
     <div className="p-inputgroup" key="search">
       <InputText placeholder="Search" />
       <Button icon="pi pi-search" className="p-button-secondary" />
     </div>,
+    <Button icon="pi pi-bell" className='p-button-secondary' key='notfications'>
+      <Badge value='4' severity="danger"></Badge>
+    </Button>
   ];
-
+  const sidebarItems = [
+    {
+      label: 'Dashboard',
+      to: '/dashboard',
+      component: Link,
+    },
+    {
+      label: 'Trending Market',
+      to: '/trending-market',
+      component: Link,
+    },
+    {
+      label: 'Analyze',
+      items: [
+        {
+          label: ' Historique',
+          to: '/history',
+          component: Link,
+        },
+        // ... other Analyze sub-menu items
+      ],
+    },
+    {
+      label: 'Exchange',
+      items: [
+        {
+          label: 'Bitcoin',
+          icon: 'pi pi-fw pi-bitcoin',
+          command: () => {
+            handleClose();
+          },
+        },
+        // ... other Exchange items
+      ],
+    },
+    {
+      label: 'Setting',
+      to: '/setting',
+      component: Link,
+    },
+    {
+      label: 'Profile',
+      icon: 'pi pi-fw pi-user',
+      command: () => {
+        handleClose();
+      },
+    },
+  ];
   return (
-    <>
-      <Toolbar className="p-mb-6 p-d-flex p-jc-space-between " right={rightItems} />
-      <Menu
-        id="menu-appbar"
-        ref={menuRef} // pass the ref here
-        model={[
-          {
-            label: 'Dashboard',
-          },
-          {
-            label: 'Trending Market',
-          },
-          {
-            label: 'Analyze',
-            items: [
-              {
-                label: '00 Historique',
-                component: Link,
-                to: '/history',
-              },
-              {
-                label: 'Token',
-                icon: 'pi pi-fw pi-file',
-              },
-              {
-                label: 'Symbol',
-                icon: 'pi pi-fw pi-hashtag',
-              },
-              {
-                label: '24H Change',
-                icon: 'pi pi-fw pi-history',
-              },
-              {
-                label: 'Last Price',
-                icon: 'pi pi-fw pi-dollar',
-              },
-              {
-                label: 'Market Cap',
-                icon: 'pi pi-fw pi-chart-bar',
-              },
-            ],
-          },
-          {
-            label: 'Exchange',
-            items: [
-              {
-                label: 'Bitcoin',
-                icon: 'pi pi-fw pi-bitcoin',
-                command: () => {
-                  handleClose();
-                },
-              },
-              {
-                label: 'Ethereum',
-                icon: 'pi pi-fw pi-ethereum',
-                command: () => {
-                  handleClose();
-                },
-              },
-              {
-                label: 'Terra',
-                icon: 'pi pi-fw pi-trending-up',
-                command: () => {
-                  handleClose();
-                },
-              },
-              {
-                label: 'BNB',
-                icon: 'pi pi-fw pi-bank',
-                command: () => {
-                  handleClose();
-                },
-              },
-              {
-                label: 'Cardano',
-                icon: 'pi pi-fw pi-credit-card',
-                command: () => {
-                  handleClose();
-                },
-              },
-            ],
-          },
-          {
-            label: 'Setting',
-          },
-          {
-            label: 'Profile',
-            icon: 'pi pi-fw pi-user',
-            command: () => {
-              handleClose();
-            },
-          },
-        ]}
-        popup
-        appendTo="self"
-        onHide={handleClose}
-      />
-      {children}
-    </>
+    <div className="layout">
+      <Toolbar className=" p-mb-6 p-d-flex p-jc-space-between "
+        right={rightItems}>
+        <div className='p-d-flex p-ai-center'>
+          <h3>Welcome Back Mohamed!</h3>
+          <i className='pi pi-user p-ml-3 p-mr-3'></i>
+        </div>
+      </Toolbar>
+      <Sidebar visible={visible} style={{ width: '250px' }} baseZIndex={1000000}
+        onHide={() => setVisible(false)}>
+        <Menu model={sidebarItems} />
+      </Sidebar>
+      <Button type="button" icon="pi pi-bars" onClick={() => setVisible(true)} />
+      <div className='main-content'> {children}
+      </div>
+    </div>
   );
 };
 Layout.propTypes = {
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 import Layout from './components/Layout'
+import 'primereact/resources/themes/arya-blue/theme.css';
+import 'primeicons/primeicons.css';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
