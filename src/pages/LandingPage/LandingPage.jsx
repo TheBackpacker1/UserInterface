394ed8faf133922114550a6846ae803d679e0e85
mohamed louati <mohamedlouati002@gmail.com>
2024-09-17 23:17:08 +0100

    Last Changes

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,7 @@
 import Navbar from './NavBar'
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css'; 
 
 
 const LandingPage = () => {
--- src/pages/LandingPage/NavBar.jsx
+++ src/pages/LandingPage/NavBar.jsx
@@ -1,99 +1,32 @@
-import { useState } from 'react'
-import {
-    AppBar,
-    Toolbar,
-    IconButton,
-    Menu, MenuItem,
-    useMediaQuery,
-    useTheme,
-    Typography,
-    createTheme,
-    ThemeProvider
-} from '@mui/material'
-import { makeStyles } from '@mui/styles'
-import MenuIcon from '@mui/icons-material/Menu';
-
-
-
-const useStyles = makeStyles((theme) => ({
-    appBar: {
-        backgroundColor: '#111111',
-        color: '#ffffff',
+import { Menubar, MenuItem } from 'primereact/menubar';
+function Navbar() {
+  const model = [
+    {
+      label: 'CryptoCap',
+      icon: 'pi pi-home',
+      items: [
+        { label: 'Home' },
+        { label: 'Businesses' },
+        { label: 'Trade' },
+        { label: 'Market' },
+        { label: 'Learn' },
+      ],
     },
-    appBarTitle: { flexGrow: 1, },
-    navLink: {
-        color: '#ffffff',
-        textDecoration: 'none',
-        marginRight: theme.spacing(2),
+    {
+      label: 'EN',
+      icon: 'pi pi-globe',
+      items: [
+        { label: 'FR' },
+        { label: 'DE' },
+        { label: 'ES' },
+      ],
     },
-    navMenuIcon: {
-        marginLeft: theme.spacing(2),
-        marginRight: -theme.spacing(2),
-        color: '#ffffff',
-    },
-    navMenu: {
-    },
-}))
-const NavBar = () => {
-    const classes = useStyles();
-    const [anchorEl, setAnchorEl] = useState(null)
-    const theme = useTheme()
-    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-    const handelMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget)
-    }
-    const handelMenuClose = () => {
-        setAnchorEl(null)
-    }
-    return (
-        <ThemeProvider theme={theme}>
+    { label: 'Login', icon: 'pi pi-user' },
+  ];
 
-        <AppBar position='static' className={classes.appBar}>
-            <Toolbar>
-                <Typography variant='h6' component='div'
-                    className={classes.appBarTitle}>
-                    CryptoInvest
-                </Typography>
-                {isMobile ? (
-                    <IconButton aria-label='account of current user'
-                        aria-controls='account-menu' aria-haspopup='true'
-                        onClick={handelMenuOpen} className={classes.navMenuIcon}>
-                        <MenuIcon />
-                    </IconButton>
-                ) : (
-                    <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: 0 }}>
-                        <li> <a href="#" className={classes.navLink}>
-                            Home
-                        </a></li>
-                        <li><a href="#" className={classes.navLink}>
-                            Businesses </a></li>
-                        <li> <a href="#" className={classes.navLink}>
-                            Trade
-                        </a></li>
-                        <li> <a href="#" className={classes.navLink}>
-                            Market
-                        </a></li>
-                        <li> <a href="#" className={classes.navLink}>
-                            Learn
-                        </a></li>
-                    </ul>
-                )}
-                <Menu id='account-menu' anchorEl={anchorEl} anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                }} transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }} open={Boolean(anchorEl)} onClose={handelMenuClose}
-                    className={classes.navMenu}>
-                    <MenuItem onClick={handelMenuClose}>
-                        <a href="#" className={classes.navLink}> Login</a>
-                    </MenuItem>
-                </Menu>
-            </Toolbar>
-        </AppBar>
-        </ThemeProvider>
-
-    )
+  return (
+    <Menubar className="custom-navbar" model={model} />
+  );
 }
-export default NavBar;
+
+export default Navbar;
