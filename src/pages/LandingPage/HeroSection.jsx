0cf9bc11332f7e283b6ea9815af3fea33c449dbe
mohamed louati <mohamedlouati002@gmail.com>
2024-10-08 21:56:30 +0100

    working on getstarted button

--- src/pages/LandingPage/HeroSection.jsx
+++ src/pages/LandingPage/HeroSection.jsx
@@ -1,14 +1,42 @@
-
+import {useState} from 'react'
 import { Button } from 'primereact/button';
+
+import AuthDialog from '../AuthDialog';
+
 import './HeroSection.css'
+
 const HeroSection = () => {
+
+const [showDialog , setShowDialog]=useState(false) 
+const [isAuthenticated, setIsAuthenticated]=useState(false)
+
+
+const handleGetStarted = () => {
+  setShowDialog(true);
+};
+
+const onLoginSuccess = () => {
+  console.log('Login successful!');
+};
+
   return (
 <div className="hero-section  ">
 <div className="hero-content">
         <h1>Empower Your Crypto Investment Journey</h1>
         <p>At CryptoInvest, we provide the tools and insights to help investors make informed decisions when buying and selling cryptocurrency. Access valuable analytics and trends to navigate the dynamic world of crypto confidently.
         </p>
-        <Button className="cta-button">Get Started</Button>
+        <Button className="cta-button" onClick={handleGetStarted}>Get Started</Button>
+
+
+
+
+        <AuthDialog
+          showDialog={showDialog}
+          setShowDialog={setShowDialog}
+          onLoginSuccess={onLoginSuccess}
+          setIsAuthenticated={setIsAuthenticated}
+        />
+
       </div>
     </div>
     
